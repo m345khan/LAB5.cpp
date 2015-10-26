53285adb00a118804de6df8324bf20bfc32cc4c4
m345khan <malik.khan@ryerson.ca>
2015-10-26 16:35:22 -0400

    added 1 more

---
+++ LAB5/src/LAB5.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : LAB5.cpp
+// Author      : Malik
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout<<"hiii";
+	cout<<"HIIII";
+	return 0;
+}
